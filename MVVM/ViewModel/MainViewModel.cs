86b82779ec2778aeb3b84d3c5cc0f0633380a60b
Jackie Cai <jackiecai2006@outlook.com>
2022-02-21 20:47:01 -0700

    Day 03

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Vinyl_Flare.Core;
+
+namespace Vinyl_Flare.MVVM.ViewModel
+{
+
+    internal class MainViewModel: Observable_Object // always showing, calls upon other views
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand LibraryViewCommand { get; set; }
+        public RelayCommand FactoryViewCommand { get; set; }
+
+        public HomeViewModel HomeVm{ get; set; }
+        public LibraryViewModel LibraryVm { get; set; }
+        public FactoryViewModel FactoryVm{ get; set; }
+
+        private object _currentView;
+
+        
+        public object CurrentView // current view object
+        {
+            get { return _currentView; }
+            set 
+            { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            // constructing default variables, declaration
+            HomeVm = new HomeViewModel();
+            LibraryVm = new LibraryViewModel();
+            FactoryVm = new FactoryViewModel();
+
+            CurrentView = HomeVm;
+
+            // Commands to switch tabs
+            HomeViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = HomeVm;
+            });
+
+            LibraryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = LibraryVm;
+            });
+
+            FactoryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = FactoryVm;
+            });
+        }
+    }
+}
