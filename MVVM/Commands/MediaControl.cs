ba6f90d8a17d478d892fc4d4cc42ae8956ab3b32
Jackie Cai <jackiecai2006@outlook.com>
2022-08-20 21:18:33 -0600

    Day 24
    got the play and pause working, and next song working working on volume slider

---
+++ MVVM/Commands/MediaControl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Vinyl_Flare.MVVM.Commands
+{
+    public class MediaControl : CommandBase
+    {
+
+        public MediaPlayer MediaPlayer;
+        private readonly string Command;
+
+
+        public MediaControl(MediaPlayer mediaPlayer, string command)
+        {
+            MediaPlayer = mediaPlayer;
+            Command = command;
+        }
+        
+        
+        public override void Execute(object parameter)
+        {
+            if (Command == "pause")
+            {
+                MediaPlayer.Pause();
+            } else if (Command == "play")
+            {
+                MediaPlayer.Play();
+            }
+        
+        }
+
+
+
+
+
+    }
+}
--- MVVM/ViewModel/PlaySongViewModel.cs
+++ MVVM/ViewModel/PlaySongViewModel.cs
@@ -1,17 +1,15 @@
 ﻿using NavigationMVVM.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
+using Vinyl_Flare.MVVM.Commands;
 
 namespace Vinyl_Flare.MVVM.ViewModel
 {
     public class PlaySongViewModel : ViewModelBase
     {
-        private MediaPlayer mediaPlayer = new(); // initializing mp3 player
+        public MediaPlayer mediaPlayer = new(); // initializing mp3 player
 
         private string _lblStatus; // property for the label to be binded
         public string lblStatus 
@@ -29,13 +27,23 @@ namespace Vinyl_Flare.MVVM.ViewModel
         public string AlbumName => _album.AlbumName;
         public string AlbumImage => _album.AlbumCoverURL;
 
+        public ICommand Pause { get; }
+        public ICommand Play { get; }
+        //     public ICommand Pause { get; }
+
+        private int SongPlaying;
+
 
         public PlaySongViewModel(Album album) // default parameter for start
         {
             _album = album; // SHIT WORKS NOW!!!!!! - AUGUST 19TH 2022
 
-            mediaPlayer.Open(new Uri(_album.Songs[4].URL)); // taking url from first song
+            SongPlaying = 0;
+            mediaPlayer.Open(new Uri(_album.Songs[SongPlaying].URL)); // taking url from first song
             mediaPlayer.Play();
+
+            Pause = new MediaControl(mediaPlayer, "pause"); // Initializing media control commands
+            Play = new MediaControl(mediaPlayer, "play");
 
             DispatcherTimer timer = new(); // initializing timer for song
             timer.Interval = TimeSpan.FromSeconds(1);
@@ -44,6 +52,8 @@ namespace Vinyl_Flare.MVVM.ViewModel
         }
 
         
+       
+        
 
         void timer_Tick(object sender, EventArgs e) // function for changing timer display
         {
@@ -51,6 +61,18 @@ namespace Vinyl_Flare.MVVM.ViewModel
                 lblStatus = String.Format("{0} / {1}", mediaPlayer.Position.ToString(@"mm\:ss"), mediaPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
             else
                 lblStatus = "No file selected...";
+
+            if (mediaPlayer.Position.TotalMilliseconds == mediaPlayer.NaturalDuration.TimeSpan.TotalMilliseconds)
+            {
+                mediaPlayer.Stop(); // switching to next song after one finishes
+                if (SongPlaying <= _album.Songs.Count)
+                { // checking to make sure there are more songs
+                    SongPlaying += 1;
+                    mediaPlayer.Open(new Uri(_album.Songs[SongPlaying].URL));
+                    mediaPlayer.Play();
+                }
+            }
+
         }
 
         private void OnlblStatusPropertyChanged()
